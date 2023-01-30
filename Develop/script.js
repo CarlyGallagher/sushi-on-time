71f7daf3f2f1d040da505b07359afe199949c850
CarlyGallagher <jagllaghercarly3@yahoo.com>
2023-01-29 19:03:04 -0800

    got local storage working further

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,24 @@
 
 
-$(function () {
+$(document).ready(function () {
+
   var current = dayjs();
   $('#currentDay').text(current.format('[Today is] MMMM dddd D, YYYY'));
+
+  $('.saveBtn').click(function () {
+    var timeBlockId = $(this).parent().attr('id');
+    localStorage.setItem('timeBlockId', timeBlockId);
+
+    value = $(this).siblings("textarea").val();
+    textString = $(this).siblings("div").text();
+    localStorage.setItem(textString, JSON.stringify(value));
+
+    
+    
+  });
+
+let currentDate = new Date();
+let time = currentDate.getHours() + ":" + currentDate.getMinutes();
+console.log(time);
 
 });
